442c39ef2faa1046f12a1571967a327af8285133
J0113boy <keskitalo_6@hotmail.com>
2024-06-03 15:27:21 +0200

    skeleton for reviewControllers added

---
+++ controllers/reviewControllers.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const Review = require('../models/reviewModel')
+
+exports.createReview = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+exports.getAllReviews = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+exports.getReviewById = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+exports.updateReviewById = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+exports.deleteReviewById = async (req, res) => {
+    try {
+
+    } catch(error) {
+        res.status(400).send(error)
+    }
+}
