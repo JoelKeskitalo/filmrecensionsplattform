033ec1692fda84999bc18983ef9647527935c227
J0113boy <keskitalo_6@hotmail.com>
2024-06-06 15:06:49 +0200

    added adminAuthentication to relevent endpoints

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const movieController = require('../controllers/movieControllers')
-const authentication = require('../middleware/authentication')
+const { authentication, adminAuthentication } = require('../middleware/authentication')
 
 // base: http://localhost:6000/moviereview-api/movies
 
-router.post('/', authentication, movieController.addMovie)
+router.post('/', authentication, adminAuthentication, movieController.addMovie)
 router.get('/', movieController.getAllMovies)
 router.get('/:id', movieController.getMovieById)
 
-router.put('/:id', authentication, movieController.updateMovieById)
+router.put('/:id', authentication, adminAuthentication, movieController.updateMovieById)
 
 router.get('/:id/reviews', movieController.getAllReviewsById)
-router.delete('/:id', authentication, movieController.deleteMovieById) 
+router.delete('/:id', authentication, adminAuthentication, movieController.deleteMovieById) 
 
 module.exports = router
